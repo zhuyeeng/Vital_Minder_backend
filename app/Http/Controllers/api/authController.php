df7d05b30404f4d6b54b108e8715d0b936f50232
zhuyeeng <zhuyeeng0524@gmail.com>
2024-06-05 18:40:26 +0800

    update controller

--- app/Http/Controllers/api/authController.php
+++ app/Http/Controllers/api/authController.php
@@ -5,34 +5,35 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Validator;
-
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class authController extends Controller
 {
-    function register(Request $request){
-        $validator = Validator::make($request->all(),[
-            'name'=>'required',
-            'email'=>'required',
-            'password'=>'required'
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6'
         ]);
 
-        if($validator->fail()){
+        if ($validator->fails()) {
             return response()->json([
-                'status'=>'false',
-                'data'=>$validator->errors()
-            ]);
-        }else{
+                'status' => 'false',
+                'data' => $validator->errors()
+            ], 422);
+        } else {
             $user = User::create([
-                'name'->$request->name,
-                'email'->$request->email,
-                'password'->$request->password
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
             ]);
 
             return response()->json([
-                'status'=>'true',
-                'message'=>'User Register Successful',
-                'data'=>$user->createToken('register_token')->plainTextToken
+                'status' => 'true',
+                'message' => 'User Register Successful',
+                'data' => $user->createToken('register_token')->plainTextToken
             ]);
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\authController;
 
-Route::middleware('auth:scanctum')->get('/user', function (Request $request){
+Route::middleware('auth:sanctum')->get('/user', function (Request $request){
     return $request->user();
 });
 
-Route::post('registeruser', [AuthController::class, 'register']);
+Route::post('/registeruser', [authController::class, 'register']);
