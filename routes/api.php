8b6796296cdf13f8d6f9f0d9b87428777dd7b09c
zhuyeeng <zhuyeeng0524@gmail.com>
2024-07-07 15:13:19 +0800

    creating controller

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\AuthController;
+use App\Http\Controllers\api\fetchUserController;
 
 Route::post('/registeruser', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -10,3 +11,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::get('/fetchStaff', [fetchUserController::class, 'getAllMedicalStaff']);
